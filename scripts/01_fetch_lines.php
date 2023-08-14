5b517bf30c9b879a0ccf3912312a90d9ad50a633
kiang <kiange@gmail.com>
2023-08-14 23:48:25 +0800

    generate json files

--- scripts/01_fetch_lines.php
+++ scripts/01_fetch_lines.php
@@ -11,15 +11,21 @@ $csvPath = $baseDir . '/docs/data/csv';
 $listFh = fopen($csvPath . '/list.csv', 'r');
 fgetcsv($listFh, 2048);
 while ($line = fgetcsv($listFh, 2048)) {
+    $jsonPath = $baseDir . '/docs/data/json/' . $line[0];
+    if (!file_exists($jsonPath)) {
+        mkdir($jsonPath, 0777, true);
+    }
     $rawPath = $baseDir . '/raw/' . $line[0];
     if (!file_exists($rawPath)) {
         mkdir($rawPath, 0777, true);
     }
     $rawHtml = $rawPath . '/' . $line[5] . '.html';
-    // $url = "https://hwms.epa.gov.tw/dispPageBox/route/routeCP.aspx?ddsPageID=LINEINFO&dbid={$line[5]}";
-    // $client->request('GET', $url);
-    // file_put_contents($rawHtml, $client->getResponse()->getContent());
+    $url = "https://hwms.epa.gov.tw/dispPageBox/route/routeCP.aspx?ddsPageID=LINEINFO&dbid={$line[5]}";
+    $client->request('GET', $url);
+    file_put_contents($rawHtml, $client->getResponse()->getContent());
     $html = file_get_contents($rawHtml);
+    $data = parseContent($html);
+
     $pagePos = strpos($html, '<ul class="pagination">');
     $pagePosEnd = strpos($html, '</ul>', $pagePos);
     $pageParts = explode("dbid={$line[5]}&p=", substr($html, $pagePos, $pagePosEnd - $pagePos));
@@ -31,13 +37,69 @@ while ($line = fgetcsv($listFh, 2048)) {
         if ($targetPage > 1 && !isset($pageDone[$targetPage])) {
             $pageDone[$targetPage] = true;
             $rawHtml = $rawPath . '/' . $line[5] . '_' . $targetPage . '.html';
-            if (!file_exists($rawHtml)) {
-                $url = "https://hwms.epa.gov.tw/dispPageBox/route/routeCP.aspx?ddsPageID=LINEINFO&dbid={$line[5]}&p={$targetPage}";
-                $client->request('GET', $url);
-                file_put_contents($rawHtml, $client->getResponse()->getContent());
-                echo "{$rawHtml}\n";
-            }
-
+            $url = "https://hwms.epa.gov.tw/dispPageBox/route/routeCP.aspx?ddsPageID=LINEINFO&dbid={$line[5]}&p={$targetPage}";
+            $client->request('GET', $url);
+            file_put_contents($rawHtml, $client->getResponse()->getContent());
+            $data2 = parseContent(file_get_contents($rawHtml));
+            $data['points'] = array_merge($data['points'], $data2['points']);
         }
     }
+    file_put_contents($jsonPath . '/' . $line[5] . '.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+}
+
+function parseContent($html)
+{
+    $data = [
+        'meta' => [],
+        'points' => [],
+    ];
+    $pos = strpos($html, '<dt>');
+    if (false !== $pos) {
+        $posEnd = strpos($html, '</dl>', $pos);
+        $lines = explode('</dd>', substr($html, $pos, $posEnd - $pos));
+        foreach ($lines as $line) {
+            $cols = explode('</dt>', $line);
+            if (count($cols) !== 2) {
+                continue;
+            }
+            foreach ($cols as $k => $v) {
+                $cols[$k] = trim(strip_tags($v));
+            }
+            $data['meta'][$cols[0]] = $cols[1];
+        }
+    }
+    $pos = strpos($html, '<div class="dataResult">');
+    if (false !== $pos) {
+        $posEnd = strpos($html, '<div class="resultArea col-sm-9">', $pos);
+        $lines = explode('</div>', substr($html, $pos, $posEnd - $pos));
+        foreach ($lines as $line) {
+            $cols = explode('</span>', $line);
+            if (count($cols) !== 3) {
+                continue;
+            }
+            foreach ($cols as $k => $v) {
+                $cols[$k] = trim(strip_tags($v));
+            }
+            $data['meta'][$cols[1]] = $cols[0];
+        }
+    }
+
+    $pos = strpos($html, '<tbody>');
+    $header = ['清運序', '清運點名稱描述', '清運時間', '一般垃圾', '廚餘回收', '資源回收'];
+    if (false !== $pos) {
+        $posEnd = strpos($html, '</tbody>', $pos);
+        $lines = explode('</tr>', substr($html, $pos, $posEnd - $pos));
+        foreach ($lines as $line) {
+            $cols = explode('</td>', $line);
+            if (count($cols) !== 7) {
+                continue;
+            }
+            array_pop($cols);
+            foreach ($cols as $k => $v) {
+                $cols[$k] = trim(strip_tags($v));
+            }
+            $data['points'][] = array_combine($header, $cols);
+        }
+    }
+    return $data;
 }
